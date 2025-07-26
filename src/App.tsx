1d8dbe982ba5f24884c32098583d45a238d6f7ab
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2025-07-26 19:02:52 -0300

    Add cooffes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { coffees } from './assets/coffees/coffees';
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <img src={coffees[1].image} alt="Americano Coffee" />
+    </>
+  );
 }
