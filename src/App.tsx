329a35e7f9b04d79086e0f0eb40c0b09660f5dcc
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2025-07-29 12:20:34 -0300

    chore: install styled-components and add theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from 'styled-components';
 import { coffees } from './assets/coffees/coffees';
+import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello world</h1>
       <img src={coffees[1].image} alt="Americano Coffee" />
-    </>
+    </ThemeProvider>
   );
 }
