8d3e7676b5181af5918de84da610989404090f42
emanuelhenrique-dev <emanuelhenriquefs@gmail.com>
2025-07-29 13:04:05 -0300

    feat: add GlobalStyle and custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import { coffees } from './assets/coffees/coffees';
+
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hello world</h1>
       <img src={coffees[1].image} alt="Americano Coffee" />
     </ThemeProvider>
